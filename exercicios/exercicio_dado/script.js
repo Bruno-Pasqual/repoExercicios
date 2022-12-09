b5dc2ebb439451ca645ceefa0e0aca6a71012450
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-09 12:20:18 -0300

    adicionando exercício dos dados

---
+++ exercicios/exercicio_dado/script.js
@@ -0,0 +1,11 @@
+/*     let imagens = [`./dice_1.gif`, `./dice_2.gif`, `./dice_3.gif`, `./dice_4.gif`, `./dice_5.gif`, `./dice_6.gif`]; */
+let aleatorio1 = Math.floor(Math.random() * 7);
+let aleatorio2 = Math.floor(Math.random() * 7);
+// Selecionando containers
+const dado1 = document.getElementById('dado1');
+const dado2 = document.getElementById('dado2');
+const jogaDado = document.getElementById('botao_jogaDado');
+//Event handlers
+jogaDado.addEventListener('click', () => {
+  alert('fui clicado');
+});
