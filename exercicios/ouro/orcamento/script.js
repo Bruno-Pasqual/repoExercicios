9e9540631def454ff8646fa64dea02d543ff077e
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-11 20:46:05 -0300

    exercício orçamento finalizado, testar

--- exercicios/ouro/orcamento/script.js
+++ exercicios/ouro/orcamento/script.js
@@ -9,41 +9,54 @@ const educacaoInput = document.getElementById('educacao_input');
 const botaoSubmit = document.getElementById('submit_button');
 //Selecionando os campos de saída
 const campoSaidas = document.querySelectorAll('.details');
-const recomendados = document.querySelectorAll('recomendado');
+const recomendados = document.querySelectorAll('.recomendado');
 
 // Contantes ----
 
 //------------------------------------//-----------------------
+console.log(recomendados);
 
 botaoSubmit.addEventListener('click', () => {
-  for (let i = 1; i < campoSaidas.length; i++) {
+  for (let i = 0; i < campoSaidas.length; i++) {
     //! variáveis ------------------------------------
     [renda, moradia, transporte, educacao] = [
       Number(rendaInput.value),
       Number(moradiaInput.value),
-      Number(transporteInput.value),
       Number(educacaoInput.value),
+      Number(transporteInput.value),
     ];
     let porcentagens = ['', 0.3, 0.2, 0.15];
     let saidas = [renda, moradia, transporte, educacao];
-    console.log(saidas);
-    let porcentagem = saidas[i].value / saidas[0];
+    let porcentagem = Number(saidas[i].value) / Number(saidas[i]);
     console.log(porcentagem);
 
     //!  Fim das variáveis ------------------------------
-    campoSaidas[i].style.color = 'red';
+    if (i === 0) {
+      campoSaidas[i].innerHTML = `<p class="details">R$: ${saidas[i]}</p>`;
+    } else {
+      campoSaidas[i].innerHTML = `Seu custo: R$ ${saidas[i]} ( ${Number(
+        (saidas[i] / rendaInput.value) * 100
+      ).toFixed(2)}%)`;
+
+      recomendados[i - 1].innerHTML = `recomendado: R$ ${
+        porcentagens[i] * rendaInput.value
+      } ( ${Number(porcentagens[i] * 100)}%)`;
+    }
   }
+  // fazerAnalise();
 });
 
 //aa
 function fazerAnalise() {
+  let saidas = [renda, moradia, transporte, educacao];
   console.log(rendaInput.value);
-  campoSaidas[0].innerHTML = `<p class="details">R$: ${rendaInput.value}</p>`;
+  //alterando valor da renda
+
   //Gasto com casa
-  campoSaidas[1].innerHTML = `Seu custo: R$ ${moradiaInput.value} ( ${
-    Number(moradiaInput.value / rendaInput.value) * 100
+  campoSaidas[0 + 1].innerHTML = `Seu custo: R$ ${saidas[1]} ( ${
+    Number(saidas[1] / rendaInput.value) * 100
   }%)`;
-  recomendados[1].innerHTML = `Seu custo: R$ ${moradiaInput.value} ( ${
+  recomendados[0].innerHTML = `recomendado: R$ ${saidas[1]} ( ${
     Number(moradiaInput.value / rendaInput.value) * 100
   }%)`;
 }
