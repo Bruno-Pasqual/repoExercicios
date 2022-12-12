32f8e3a1edc572c169f49353cf59ea125caea133
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-12 06:39:47 -0300

    ajustando design, finalizando lógica

--- exercicios/imc/script.js
+++ exercicios/imc/script.js
@@ -3,6 +3,7 @@
 const submitButton = document.getElementById('submit_button');
 const mensagensErro = document.querySelectorAll('.mensagem_erro');
 const entradas = document.querySelectorAll('.input');
+const containerFormulario = document.getElementById('container_formulario');
 //Saídas (segunda tela)
 const containerResultados = document.querySelector('.container_resultados');
 const resultadoIMC = document.getElementById('resultado_imc');
@@ -16,33 +17,33 @@ function checarEntradas() {
   entradas.forEach((input, index) => {
     //Sem valor algum
     if (input.value === '') {
-      input.style.border = '3px solid red';
+      input.style.border = '2px solid red';
       mensagensErro[index].textContent = 'Não deixe campos em branco';
       mensagensErro[index].style.display = 'block';
       return false;
       //Peso superior a 400 kgs (pessoa mais pesada no planeta está abaixo)
     } else if (index === 0 && Number(input.value) > 400) {
-      input.style.border = '3px solid red';
+      input.style.border = '2px solid red';
       mensagensErro[index].textContent = 'Digite um peso válido (max 400kgs)';
       mensagensErro[index].style.display = 'block';
       return false;
       //Checando se a pessoa colocou o valor em CMs
     } else if (index === 1 && Number(input.value) > 3) {
-      input.style.border = '3px solid red';
+      input.style.border = '2px solid red';
       mensagensErro[index].textContent =
         'Coloque uma altura válida (metros) (max: 3m)';
       mensagensErro[index].style.display = 'block';
       return false;
       //Valor igual a 0
     } else if (Number(input.value) <= 0) {
-      input.style.border = '3px solid red';
+      input.style.border = '2px solid red';
       mensagensErro[index].textContent =
         'Devem ser positivos e diferentes de 0';
       mensagensErro[index].style.display = 'block';
       return false;
     } else {
       mensagensErro[index].style.display = 'none';
-      input.style.border = '1px solid lightgreen';
+      input.style.border = '2px solid lightgreen';
       return true;
     }
   });
@@ -66,9 +67,9 @@ function classificarIMC(imc) {
 submitButton.addEventListener('click', () => {
   // checarEntradas();
   if (!checarEntradas()) {
+    //definindo valor do imc
     let IMC =
       Number(entradas[0].value) / Math.pow(Number(entradas[1].value), 2);
-    console.log(IMC);
     resultadoIMC.textContent = IMC.toFixed(2);
   } else {
     alert('Houve um erro, tente novamente ');
