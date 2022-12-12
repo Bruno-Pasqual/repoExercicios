78f31a262679ea41d57e1c01665fd61aeceba5ce
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-12 02:51:01 -0300

    CSS ok, começando a lógica no javascript

---
+++ exercicios/imc/script.js
@@ -0,0 +1,9 @@
+//! Selecionando elementos --------------------------
+
+const pesoInput = document.getElementById('peso_input');
+const alturaInput = document.getElementById('altura_input');
+const submitButton = document.getElementById('submit_button');
+
+//! Functions ---------------------------------------------
+
+//! Event handlers ---------------------------------------------
