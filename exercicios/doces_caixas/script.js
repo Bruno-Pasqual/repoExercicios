4aaca302b9aedc181910d5a8ad0f64137df2333b
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-15 20:36:39 -0300

    refazendo exercicio doces e caixas e também criação de pasta do próximo exercício

---
+++ exercicios/doces_caixas/script.js
@@ -0,0 +1,35 @@
+// asadasdasdsdadads
+
+function checarEntradas() {
+  console.log(entradas);
+  //---- checando o peso
+  entradas.forEach((input, index) => {
+    //Sem valor algum
+    if (input.value === '') {
+      input.style.border = '2px solid red';
+      mensagensErro[index].textContent = 'Não deixe campos em branco';
+      mensagensErro[index].style.display = 'block';
+      //Peso superior a 400 kgs (pessoa mais pesada no planeta está abaixo)
+    } else if (index === 0 && Number(input.value) > 400) {
+      input.style.border = '2px solid red';
+      mensagensErro[index].textContent = 'Digite um peso válido (max 400kgs)';
+      mensagensErro[index].style.display = 'block';
+      //Checando se a pessoa colocou o valor em CMs
+    } else if (index === 1 && Number(input.value) > 3) {
+      input.style.border = '2px solid red';
+      mensagensErro[index].textContent =
+        'Coloque uma altura válida (metros) (max: 3m)';
+      mensagensErro[index].style.display = 'block';
+      //Valor igual a 0
+    } else if (Number(input.value) <= 0) {
+      input.style.border = '2px solid red';
+      mensagensErro[index].textContent =
+        'Devem ser positivos e diferentes de 0';
+      mensagensErro[index].style.display = 'block';
+    } else {
+      mensagensErro[index].style.display = 'none';
+      input.style.border = '2px solid lightgreen';
+      houveErro = true;
+    }
+  });
+}
