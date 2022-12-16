be725b233e08083bb1ab5d37386dc0a0f5ac7b55
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-15 23:55:38 -0300

    trabalhando na lógica

--- exercicios/doces_caixas/script.js
+++ exercicios/doces_caixas/script.js
@@ -1,8 +1,23 @@
 //! Selecionando os elementos  ------------------------------------------------
 
+// Seleção das entradas
+
 const entradaDeDados = document.querySelectorAll('.entrada_dados');
 const mensagensErro = document.querySelectorAll('.mensagem_erro');
 const submitButton = document.getElementById('submit_button');
+
+// Seleção das saídas
+
+const informacaoDoces = document.getElementById('informacoes_doces');
+const informacoesCapacidadeCaixa = document.getElementById(
+  'informacoes_capacidade_caixa'
+);
+const saidaCaixasCompletas = document.getElementById(
+  'quantidade_caixas_fechadas'
+);
+const saidaCaixasIncompletas = document.getElementById(
+  'quantidade_caixas_incompletas'
+);
 
 //! Variaveís de controle -----------------------------------------------------
 
@@ -36,18 +51,24 @@ function checarEntradas() {
 function realizarLogica() {
   //
   let quantidadeDoces = Number(entradaDeDados[0].value);
-  let capacidadeCaixas = Number(entradaDeDados[1].value);
-  let caixasCompletas = 0;
+  let capacidadeDasCaixas = Number(entradaDeDados[1].value);
+  let caixasCompletas = Math.floor(quantidadeDoces / capacidadeDasCaixas);
+  let quantidadeDocesSobraram = quantidadeDoces % caixasCompletas;
   //
-  for (i = 1; quantidadeDoces >= capacidadeCaixas; i++) {
-    caixasCompletas++;
-  }
-  console.log(caixasCompletas);
+
+  console.log(
+    `quantidade de doces: ${quantidadeDoces}\nCapacidade das caixas: ${capacidadeDasCaixas}\nCaixas completas: ${caixasCompletas}\nDoces que sobraram: ${quantidadeDocesSobraram}`
+  );
+
+  informacaoDoces.textContent = quantidadeDoces;
+  informacoesCapacidadeCaixa.textContent = capacidadeDasCaixas;
+  saidaCaixasCompletas.textContent = `${caixasCompletas} caixas completas`;
+  quantidadeDocesSobraram != 0 ? saidaCaixasIncompletas.textContent= : '';
 }
 
 //! Event handlers -------------------------------------------------------------
 
 submitButton.addEventListener('click', () => {
   checarEntradas();
-  realizarLogica();
+  houveErro === false ? realizarLogica() : '';
 });
