90304b113ada5a6943a97f6c5f59b5970d133f1d
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2022-12-16 21:00:05 -0300

    solução provisória para o exercicio do caixa

--- exercicios/caixaEletronico/script.js
+++ exercicios/caixaEletronico/script.js
@@ -4,38 +4,82 @@ const valorASacar = document.getElementById('valor_a_sacar');
 const mensagemErro = document.getElementById('mensagem_erro');
 const submitButton = document.getElementById('submit_button');
 
+console.log(valorASacar, mensagemErro, submitButton);
+
+//! Variáveis ------------------------------------------------------
+
+let notas100 = 0;
+let notas50 = 0;
+let notas20 = 0;
+let notas10 = 0;
+let resto;
+let valor;
+let tudoCerto = false;
+
 //! Funções ---------------------------------------------------------
 
-function validarEntrada() {
-  if ((valorASacar.value = '')) {
+function checarDado() {
+  if (valorASacar.value == '') {
     mensagemErro.style.display = 'block';
-    mensagemErro.textContent = 'Digite algum valor';
-    valorASacar.style.border = '1px solid red';
+    mensagemErro.textContent = 'Não pode ficar em branco';
+    valorASacar.style.border = '2px solid red';
   } else if (Number(valorASacar.value) <= 0) {
     mensagemErro.style.display = 'block';
-    mensagemErro.textContent = 'O valor deve ser positivo e diferente de 0';
+    mensagemErro.textContent = 'Deve ser positivo e diferente de 0';
+    valorASacar.style.border = '2px solid red';
+  } else if (!Number.isInteger(Number(valorASacar.value))) {
+    mensagemErro.style.display = 'block';
+    mensagemErro.textContent = 'Digite um valor inteiro';
+    valorASacar.style.border = '2px solid red';
+  } else if (Number(valorASacar.value) % 10 != 0) {
+    mensagemErro.style.display = 'block';
+    mensagemErro.textContent = 'Apenas valores multiplos de 10';
+    valorASacar.style.border = '2px solid red';
+  } else {
+    mensagemErro.style.display = 'none';
+    valorASacar.style.border = '2px solid green';
+    tudoCerto = true;
   }
 }
 
+function quantasNotas(valor) {
+  console.log(`valor dentro do quantasNotas ${valor}`);
+  checarNotas(valor);
+}
+
+function checarNotas(valor) {
+  console.log('checarNotas', valor);
+  if (valor >= 100) {
+    notas100 = Math.floor(valor / 100);
+    valor = valor % 100;
+    resto != 0 ? checarNotas(valor) : '';
+    // valor != 0 ?
+  } else if (valor >= 50) {
+    notas50 = Math.floor(valor / 50);
+    valor = valor % 50;
+    valor != 0 ? checarNotas(valor) : '';
+  } else if (valor >= 20) {
+    notas20 = Math.floor(valor / 20);
+    valor = valor % 20;
+    valor != 0 ? checarNotas(valor) : '';
+  } else if (valor >= 10) {
+    notas10 = Math.floor(valor / 10);
+    valor = valor % 10;
+    valor != 0 ? checarNotas(valor) : '';
+  }
+}
 //! Event Handlers --------------------------------------------------
 
-/* entradaDeDados.forEach((entrada_dados, index) => {
-  if (entrada_dados.value === '') {
-    mensagensErro[index].style.display = 'block';
-    mensagensErro[index].textContent = 'Não deixe campos em branco';
-    entrada_dados.style.border = '2px solid red';
-  } else if (Number(entrada_dados.value) <= 0) {
-    mensagensErro[index].style.display = 'block';
-    mensagensErro[index].textContent =
-      'Digite um valor positivo e diferente de 0';
-  } else if (!Number.isInteger(Number(entrada_dados.value))) {
-    mensagensErro[index].style.display = 'block';
-    mensagensErro[index].textContent = 'Digite um valor inteiro';
-    entrada_dados.style.border = '2px solid red';
+submitButton.addEventListener('click', () => {
+  let valor = Number(valorASacar.value);
+  checarDado();
+  if (tudoCerto === true) {
+    quantasNotas(valor);
+    alert(
+      `Notas para pagar o valor: \n\nNotas 100: ${notas100}\nNotas 50: ${notas50}\nNotas 20: ${notas20}\nNotas 10: ${notas10}`
+    );
+    alert('Recarregue a página para testar novamente');
   } else {
-    mensagensErro[index].style.display = 'none';
-    entrada_dados.style.border = '2px solid green';
-    houveErro = false;
+    alert('houve algum erro');
   }
 });
- */
