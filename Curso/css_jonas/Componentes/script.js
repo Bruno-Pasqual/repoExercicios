84ce0d42f733d9eadb3e257345d366b558ee68ab
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-01-10 14:04:31 -0300

    pronto

--- Curso/css_jonas/Componentes/script.js
+++ Curso/css_jonas/Componentes/script.js
@@ -5,6 +5,7 @@ const numerosOrganizadores = document.querySelectorAll('.numero_organizador');
 const topicosDoBox = document.querySelectorAll('.assunto_da_div');
 const segundaLinha = document.querySelectorAll('.segunda_linha');
 const containerBoxes = document.querySelectorAll('.dropbox_container');
+const imagensSeta = document.querySelectorAll('.arrow_img');
 
 //! Event handlers ----------------------------------------------------
 
@@ -12,5 +13,6 @@ dropBoxes.forEach((elemento, index) => {
   elemento.addEventListener('click', () => {
     segundaLinha[index].classList.toggle('hidden');
     containerBoxes[index].classList.toggle('ativo');
+    imagensSeta[index].classList.toggle('ativo');
   });
 });
